f9f5e0dd0506b331c7ea83d936d468a6da14e679
Omkar <omkarkadamofficial>
2025-04-26 21:20:45 +0530

    made changes in runner file

--- etl-bdd-framework/src/test/java/runner/TestRunner.java
+++ etl-bdd-framework/src/test/java/runner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-    features = "src/test/java/features",
+    features = "src/test/resources/features",
     glue = {"stepDef"},
     plugin = {"pretty", "html:target/cucumber-reports"},
     monochrome = true,
