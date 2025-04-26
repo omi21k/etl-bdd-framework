e40613becd72713051b3f36a00e6005dd17323bf
Omkar <omkarkadamofficial>
2025-04-26 23:18:05 +0530

    code updated

--- etl-bdd-framework/src/main/java/utilities/DBUtils.java
+++ etl-bdd-framework/src/main/java/utilities/DBUtils.java
@@ -5,26 +5,15 @@ import java.sql.*;
 public class DBUtils {
 	
 	
-	static DBCredentials sourceDB;
 
-	static String dbUrl;
-	static String user;
-	static String pass;
-
-	static {
-	    sourceDB = DBUtils.getDBCredFromConfig(true);
-	    dbUrl = sourceDB.getDbUrl();
-	    user = sourceDB.getUsername();
-	    pass = sourceDB.getPassword();
-	}
 
 	// Now you can use dbUrl, username, password wherever needed!
 
 
     public static DBCredentials getDBCredFromConfig(boolean useSource) {
         String dbUrl = useSource ? ConfigReader.get("source.db.url") : ConfigReader.get("target.db.url");
-        String user = ConfigReader.get("db.username");
-        String pass = ConfigReader.get("db.password");
+        String user = ConfigReader.get("db.hr.username");
+        String pass = ConfigReader.get("db.hr.password");
 
         return new DBCredentials(dbUrl, user, pass);
     }
